2bcf404bc2de7d128d4e7a33de37c40b5412f46d
sidneyemeka <nnaemekasidney@gmail.com>
2024-11-23 10:21:00 +0100

    extract deetails from privateKey

--- lib/sendetherpage.dart
+++ lib/sendetherpage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:web_socket_channel/io.dart';
 
 class Sendetherpage extends StatefulWidget {
   const Sendetherpage({super.key});
@@ -8,6 +11,22 @@ class Sendetherpage extends StatefulWidget {
 }
 
 class _SendetherpageState extends State<Sendetherpage> {
+  String remoteProcedureCallUrl = "http://127.0.0.1:7545"; //Ganache remote server
+  String webSocketUrl = "ws://127.0.0.1:7545/"; // Web socket to track changes in real time
+
+  void sendCrypto() {
+    //setting up a Webb3client which will make calls using the Http Client() to the RPC. then the web socket connector for websocket
+    Web3Client myClient = Web3Client(remoteProcedureCallUrl, Client(),
+        socketConnector: ()=> IOWebSocketChannel.connect(webSocketUrl).cast<String>());
+
+    //User Details Extraction
+    String fromPrivateKey = "0x4d2c087c52d47c5ac12f0498509dbb9844631a833e752824f03eb7e731a1ee80";
+    Credentials credentials =  EthPrivateKey.fromHex(fromPrivateKey);
+    EthereumAddress fromAddress = credentials.address;
+
+    print(fromAddress);
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,14 +34,14 @@ class _SendetherpageState extends State<Sendetherpage> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.deepPurple,
-        child: Column(
+        child: const Column(
           children: [
-            Text("Ground")
+            Text("Web 3", style: TextStyle(fontSize: 30, color: Colors.white),),
           ],
         ),
       ),),
-      floatingActionButton: FloatingActionButton(backgroundColor: Colors.black,onPressed: (){},
-      child: Icon(Icons.send_outlined,color: Colors.white,)),
+      floatingActionButton: FloatingActionButton(backgroundColor: Colors.black,onPressed: sendCrypto,
+      child: const Icon(Icons.send_outlined,color: Colors.white,)),
     );
   }
 }
