bcd7b77f4bfc0c8a8b96e68e6ea0e9018daedd19
sidneyemeka <nnaemekasidney@gmail.com>
2024-11-23 11:02:59 +0100

    sent and received eth

--- lib/sendetherpage.dart
+++ lib/sendetherpage.dart
@@ -19,14 +19,21 @@ class _SendetherpageState extends State<Sendetherpage> {
     Web3Client myClient = Web3Client(remoteProcedureCallUrl, Client(),
         socketConnector: ()=> IOWebSocketChannel.connect(webSocketUrl).cast<String>());
 
-    //User Details Extraction
-    String fromPrivateKey = "0x4d2c087c52d47c5ac12f0498509dbb9844631a833e752824f03eb7e731a1ee80";
+    //User Details Extraction from  private key
+    String fromPrivateKey = "0x77e0576d44373138daabd3a0be493737145e3ea79898d8c3f9f59af037ed8e1a";
     Credentials credentials =  EthPrivateKey.fromHex(fromPrivateKey);
-    EthereumAddress fromAddress = credentials.address;
+    EthereumAddress fromAddress =  credentials.address;
+    EthereumAddress toAddress = EthereumAddress.fromHex("0x880Bc684Af23f25a633A6Ef556627808b0fEA0AB");
+    EtherAmount amountToSend =  EtherAmount.fromInt(EtherUnit.ether,90);
 
-    print(fromAddress);
+    //Send Ethereum
+    myClient.sendTransaction(credentials, Transaction(from: fromAddress, to: toAddress, value:amountToSend,),chainId: 1337);  //always remember to add chain id
+
+   // print("sent");
 
   }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
